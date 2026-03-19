0ebe8fad0b68da1c0be966963c4dc177dbb419b0
yukinko267 <t2252100147@stu.kanazawa-u.ac.jp>
2026-03-19 16:52:54 +0900

    Revice server

--- server/app.js
+++ server/app.js
@@ -1,5 +1,6 @@
 // server/app.js
-require('dotenv').config({ path: './server/.env' });
+//require('dotenv').config({ path: './server/.env' });
+require('dotenv').config();
 // console.log(process.env.TEST);
 
 const express = require('express');
@@ -9,7 +10,7 @@ const app = express();
 
 // CORS対応
 app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', 'http://localhost:3000');
+  res.header('Access-Control-Allow-Origin', '*');
   res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
   res.header('Access-Control-Allow-Headers', 'Content-Type');
   next();
@@ -128,6 +129,8 @@ app.get("/api/nearby", async (req, res) => {
   }
 });
 
-app.listen(5000, () => {
-  console.log('Server running on port 5000');
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
 });
